a215ce1a7fd113c782b1adf4786cf7f48bb39efb
yaaqin <yaqin.me@gmail.com>
2023-11-10 10:28:32 +0700

    [ADD] homepages

--- peworl_fe/src/pages/index.js
+++ peworl_fe/src/pages/index.js
@@ -1,118 +1,120 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section id="navbar" className="sticky top-0 bg-[white]">
+        <nav className="shadow-md shadow-[#5E50A1] flex justify-between py-[20px] container mx-auto px-[100px]">
+          <img src="/images/logo-purple.png" alt="logo"></img>
+          <div className="flex justify-between gap-[20px]">
+            <button className="border-[#5E50A1] border-[2px] px-[15px] rounded-xl py-[5px]">
+              Masuk
+            </button>
+            <button className="bg-[#5E50A1] border-[2px] px-[15px] rounded-xl py-[5px] border-[#5E50A1] text-[white]">
+              Daftar
+            </button>
+          </div>
+        </nav>
+      </section>
+      <section
+        id="content-1"
+        className="py-[70px] container mx-auto px-[100px] grid grid-cols-2"
+      >
+        <div className="flex flex-col justify-center">
+          <h1 className="text-[44px]">
+            Talenta terbaik negri untuk perubahan revolusi 4.0
+          </h1>
+          <p className="py-[20px] text-[18]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+            ipsum et dui rhoncus auctor.
+          </p>
+          <div>
+            <button className="bg-[#5E50A1] border-[2px] px-[15px] rounded-md py-[5px] border-[#5E50A1] text-[white]">
+              Mulai Dari Sekarang
+            </button>
+          </div>
         </div>
-      </div>
+        <img
+          src="/images/content-1.png"
+          height="600px"
+          width="600px"
+          alt="homeBanner"
+        ></img>
+      </section>
+      <section
+        id="content-2"
+        className="container mx-auto px-[100px] grid grid-cols-2 py-[50px]"
+      >
+        <img src="/images/content-2.png" alt="homeBanner"></img>
+        <div>
+          <h2 className="my-[30px] text-[25px]">Kenapa harus mencari tallent di peworld</h2>
+          {[
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+          ]?.map((item, key) => (
+            <div key={key} className="flex gap-[20px] mt-[15px]">
+              <img src="/images/check-purple.svg" alt="check"></img>
+              <p>{item}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section id="content-3"className="container mx-auto px-[100px] grid grid-cols-2 py-[50px]" >
+      <div>
+          <h2 className="mt-[30px] mb-[15px] text-[35px]">Skill Tallent</h2>
+          <p className="mb-[10px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
+          <div className="grid grid-cols-2">
+          {[
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+          ]?.map((item, key) => (
+            <div key={key} className="flex gap-[20px] mt-[15px]">
+              <img src="/images/check-yellow.svg" alt="check"></img>
+              <p>{item}</p>
+            </div>
+          ))}
+          {[
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet.",
+          ]?.map((item, key) => (
+            <div key={key} className="flex gap-[20px] mt-[15px]">
+              <img src="/images/check-yellow.svg" alt="check"></img>
+              <p>{item}</p>
+            </div>
+          ))}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          </div>
+        </div>
+            <img src="/images/content-3.png"></img>
+      </section>
+      <section
+        id="footer"
+        className="bg-[#5E50A1] py-[50px] container mx-auto px-[100px]"
+      >
+        <img src="/images/logo-white.png" alt="logo"></img>
+        <p className="text-[white] my-[30px]">
+          Lorem ipsum dolor sit amet, consectetur
+          <br />
+          adipiscing elit. In euismod ipsum et dui
+          <br />
+          rhoncus auctor.
+        </p>
+        <hr></hr>
+        <div className="mt-[30px] flex justify-between">
+          <p className="text-[white]">2023 Pewworld. All right reserved</p>
+          <div className="flex gap-[20px]">
+            <button className="text-[white]">Telepon</button>
+            <button className="text-[white]">Email</button>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
